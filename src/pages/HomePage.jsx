b9b851729862f0f3576a96f062f3a1eff681e4c8
Shivang Kumar <2002shiva.2002@gmail.com>
2025-09-24 15:28:47 +0530

    Search functionality done!

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,14 @@
 
-import SearchPage from './/SearchPage';
+import SearchPage from '../components/SearchComponent';
+import useSearchHandler from '../hooks/useSearchHander';
 
 const HomePage=()=>{
-    return (
-       <SearchPage/>
+  
+    const handleSearch=useSearchHandler();
+    return (  <>
+    <h1>This is the Home page</h1>
+       <SearchPage  onSearch={handleSearch}/>
+    </> 
     );
 }
 
