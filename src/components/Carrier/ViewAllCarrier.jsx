d2f2477e3c8299ac76d064cfca67774d750945b3
Shivang Kumar <2002shiva.2002@gmail.com>
2025-10-06 00:36:13 +0530

    Working on Navbar Added Dynamic dropdown and rendenring choosen Component!

---
+++ src/components/Carrier/ViewAllCarrier.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ViewAllCarrier = () => {
+  return (
+    <div>
+            <h1>This is view all carrier page</h1>
+    </div>
+  )
+}
+
+export default ViewAllCarrier
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
 
-function Header(profile) {
+function Header({ profile }) {
   return (
-    <div style={{ border: '2px solid blue'}}> 
-      <div><h3>Airline Management System</h3></div>
-      <div>Search Button</div>
-      {profile.data}
+    <div
+      style={{
+        border: '2px solid blue',
+        display: 'flex',
+        justifyContent: 'space-between', // This separates the two divs
+        alignItems: 'center',            // Optional: vertically centers the items
+        padding: '0 10px',      // Optional: adds some horizontal padding
+          backgroundColor: '#f0f8ff',          
+      }}
+    >
+      <div>
+        <h3>Airline Management System</h3>
+      </div>
+      <div>Profile Button</div>
     </div>
-  )
+  );
 }
 
 export default Header
